162a3a10c51e85f1ef9739102336451c453eef0e
aryan <aryan.jhaf5@gmail.com>
2025-08-13 13:05:13 +0530

    Added task3 pipeline

---
+++ task3.groovy
@@ -0,0 +1,28 @@
+pipeline {
+    agent any
+    stages {
+        stage('Parallel Tasks') {
+            parallel {
+                stage('Fast Task') {
+                    steps {
+                        echo 'Starting fast task...'
+                        bat 'ping -n 6 127.0.0.1 >nul'
+                        echo 'Fast task completed.'
+                    }
+                }
+                stage('Slow Task') {
+                    steps {
+                        echo 'Starting slow task...'
+                        bat 'ping -n 16 127.0.0.1 >nul'
+                        echo 'Slow task completed.'
+                    }
+                }
+            }
+        }
+    }
+    post {
+        always {
+            echo 'Pipeline finished.'
+        }
+    }
+}
